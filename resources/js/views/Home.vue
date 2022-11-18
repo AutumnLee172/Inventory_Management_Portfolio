87199976ec24a327f2d6be2a2471fb765cf5ad75
ChuaWeiHan0924 <mrchua4896@gmail.com>
2022-11-18 15:17:45 +0800

    change the card height at home page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
-                    <div class="card-body p-3" style="height: 200px;">
+                    <div class="card-body p-3 card-height">
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
@@ -37,7 +37,7 @@
             </div>
             <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
-                    <div class="card-body p-3" style="height: 200px;">
+                    <div class="card-body p-3 card-height">
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
@@ -67,7 +67,7 @@
             </div>
             <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
-                    <div class="card-body p-3" style="height: 200px;">
+                    <div class="card-body p-3 card-height">
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
@@ -98,7 +98,7 @@
             </div>
             <div class="col-xl-3 col-sm-6">
                 <div class="card">
-                    <div class="card-body p-3" style="height: 200px;">
+                    <div class="card-body p-3 card-height">
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
